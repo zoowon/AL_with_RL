892c53aecc98b904b0a74ab5ffea42802381e9a6
Joowon Park <zoowon0203@naver.com>
2025-11-24 22:55:01 +0900

    251124

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # Python
+import argparse
 import os
 import random
 
@@ -13,8 +14,7 @@ from torch.utils.data.sampler import SubsetRandomSampler
 
 # Torchvison
 import torchvision.transforms as T
-import torchvision.models as models
-from torchvision.datasets import CIFAR100, CIFAR10
+from torchvision.datasets import CIFAR100, CIFAR10, FashionMNIST
 
 # Utils
 import visdom
@@ -32,25 +32,70 @@ torch.manual_seed(0)
 torch.backends.cudnn.deterministic = True
 
 ## 데이터 로더
-# Data
-train_transform = T.Compose([
-    T.RandomHorizontalFlip(),
-    T.RandomCrop(size=32, padding=4),
-    T.ToTensor(),
-    T.Normalize([0.4914, 0.4822, 0.4465], [0.2023, 0.1994, 0.2010]) # T.Normalize((0.5071, 0.4867, 0.4408), (0.2675, 0.2565, 0.2761)) # CIFAR-100
-])
+def get_transforms(dataset_name):
+    if dataset_name in {"cifar10", "cifar100"}:
+        mean = [0.4914, 0.4822, 0.4465]
+        std = [0.2023, 0.1994, 0.2010]
+        train_transform = T.Compose([
+            T.RandomHorizontalFlip(),
+            T.RandomCrop(size=32, padding=4),
+            T.ToTensor(),
+            T.Normalize(mean, std),
+        ])
 
-test_transform = T.Compose([
-    T.ToTensor(),
-    T.Normalize([0.4914, 0.4822, 0.4465], [0.2023, 0.1994, 0.2010]) # T.Normalize((0.5071, 0.4867, 0.4408), (0.2675, 0.2565, 0.2761)) # CIFAR-100
-])
+        test_transform = T.Compose([
+            T.ToTensor(),
+            T.Normalize(mean, std),
+        ])
+    elif dataset_name == "fashionmnist":
+        mean = [0.2860, 0.2860, 0.2860]
+        std = [0.3530, 0.3530, 0.3530]
+        train_transform = T.Compose([
+            T.Resize(32),
+            T.Grayscale(num_output_channels=3),
+            T.RandomHorizontalFlip(),
+            T.RandomCrop(size=32, padding=4),
+            T.ToTensor(),
+            T.Normalize(mean, std),
+        ])
 
-cifar10_train = CIFAR10('../cifar10', train=True, download=True, transform=train_transform)
-cifar10_unlabeled   = CIFAR10('../cifar10', train=True, download=True, transform=test_transform)
-cifar10_test  = CIFAR10('../cifar10', train=False, download=True, transform=test_transform)
+        test_transform = T.Compose([
+            T.Resize(32),
+            T.Grayscale(num_output_channels=3),
+            T.ToTensor(),
+            T.Normalize(mean, std),
+        ])
+    else:
+        raise ValueError(f"지원하지 않는 데이터셋입니다: {dataset_name}")
+
+    return train_transform, test_transform
 
 
-##
+def get_datasets(dataset_name, data_root):
+    train_transform, test_transform = get_transforms(dataset_name)
+    dataset_path = os.path.join(data_root, dataset_name)
+
+    if dataset_name == "cifar10":
+        train_set = CIFAR10(dataset_path, train=True, download=True, transform=train_transform)
+        unlabeled_set = CIFAR10(dataset_path, train=True, download=True, transform=test_transform)
+        test_set = CIFAR10(dataset_path, train=False, download=True, transform=test_transform)
+        num_classes = 10
+    elif dataset_name == "cifar100":
+        train_set = CIFAR100(dataset_path, train=True, download=True, transform=train_transform)
+        unlabeled_set = CIFAR100(dataset_path, train=True, download=True, transform=test_transform)
+        test_set = CIFAR100(dataset_path, train=False, download=True, transform=test_transform)
+        num_classes = 100
+    elif dataset_name == "fashionmnist":
+        train_set = FashionMNIST(dataset_path, train=True, download=True, transform=train_transform)
+        unlabeled_set = FashionMNIST(dataset_path, train=True, download=True, transform=test_transform)
+        test_set = FashionMNIST(dataset_path, train=False, download=True, transform=test_transform)
+        num_classes = 10
+    else:
+        raise ValueError(f"지원하지 않는 데이터셋입니다: {dataset_name}")
+
+    return train_set, unlabeled_set, test_set, num_classes
+
+
 # Loss Prediction Loss
 def LossPredLoss(input, target, margin=1.0, reduction='mean'):
     assert len(input) % 2 == 0, 'the batch size is not even.'
@@ -73,11 +118,9 @@ def LossPredLoss(input, target, margin=1.0, reduction='mean'):
     return loss
 
 
-##
 # Train Utils
 iters = 0
 
-#
 def train_epoch(models, criterion, optimizers, dataloaders, epoch, epoch_loss, vis=None, plot_data=None):
     models['backbone'].train()
     models['module'].train()
@@ -133,7 +176,7 @@ def train_epoch(models, criterion, optimizers, dataloaders, epoch, epoch_loss, v
                 win=1
             )
 
-#
+
 def test(models, dataloaders, mode='val'):
     assert mode == 'val' or mode == 'test'
     models['backbone'].eval()
@@ -153,7 +196,7 @@ def test(models, dataloaders, mode='val'):
     
     return 100 * correct / total
 
-#
+
 def train(models, criterion, optimizers, schedulers, dataloaders, num_epochs, epoch_loss, vis, plot_data):
     print('>> Train a Model.')
     best_acc = 0.
@@ -181,7 +224,7 @@ def train(models, criterion, optimizers, schedulers, dataloaders, num_epochs, ep
             print('Val Acc: {:.3f} \t Best Acc: {:.3f}'.format(acc, best_acc))
     print('>> Finished.')
 
-#
+
 def get_uncertainty(models, unlabeled_loader):
     models['backbone'].eval()
     models['module'].eval()
@@ -201,60 +244,75 @@ def get_uncertainty(models, unlabeled_loader):
     return uncertainty.cpu()
 
 
-##
-# Main
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Dataset 선택을 위한 Active Learning 실험 스크립트')
+    parser.add_argument('--dataset', default='cifar10', choices=['cifar10', 'cifar100', 'fashionmnist'],
+                        help='사용할 데이터셋 이름')
+    parser.add_argument('--data-root', default='../data', help='데이터 다운로드 및 로딩 경로')
+    args = parser.parse_args()
+
+    dataset_name = args.dataset.lower()
+    data_root = args.data_root
+
+    train_set, unlabeled_set, test_set, num_classes = get_datasets(dataset_name, data_root)
+    num_train = len(train_set)
+
     vis = visdom.Visdom(server='http://localhost', port=9000)
     plot_data = {'X': [], 'Y': [], 'legend': ['Backbone Loss', 'Module Loss', 'Total Loss']}
+    checkpoint_dir = os.path.join('.', dataset_name, 'train', 'weights')
 
     for trial in range(TRIALS):
         # Initialize a labeled dataset by randomly sampling K=ADDENDUM=1,000 data points from the entire dataset.
-        indices = list(range(NUM_TRAIN))
+        indices = list(range(num_train))
         random.shuffle(indices)
         labeled_set = indices[:ADDENDUM]
-        unlabeled_set = indices[ADDENDUM:]
-        
-        train_loader = DataLoader(cifar10_train, batch_size=BATCH, 
-                                  sampler=SubsetRandomSampler(labeled_set), 
+        unlabeled_indices = indices[ADDENDUM:]
+
+        train_loader = DataLoader(train_set, batch_size=BATCH,
+                                  sampler=SubsetRandomSampler(labeled_set),
                                   pin_memory=True)
-        test_loader  = DataLoader(cifar10_test, batch_size=BATCH)
-        dataloaders  = {'train': train_loader, 'test': test_loader}
-        
+        test_loader = DataLoader(test_set, batch_size=BATCH)
+        dataloaders = {'train': train_loader, 'test': test_loader}
+
         # Model
-        resnet18    = resnet.ResNet18(num_classes=10).cuda()
+        resnet18 = resnet.ResNet18(num_classes=num_classes).cuda()
         loss_module = lossnet.LossNet().cuda()
-        models      = {'backbone': resnet18, 'module': loss_module}
+        models = {'backbone': resnet18, 'module': loss_module}
         torch.backends.cudnn.benchmark = False
 
         # Active learning cycles
         for cycle in range(CYCLES):
             # Loss, criterion and scheduler (re)initialization
-            criterion      = nn.CrossEntropyLoss(reduction='none')
-            optim_backbone = optim.SGD(models['backbone'].parameters(), lr=LR, 
-                                    momentum=MOMENTUM, weight_decay=WDECAY)
-            optim_module   = optim.SGD(models['module'].parameters(), lr=LR, 
-                                    momentum=MOMENTUM, weight_decay=WDECAY)
+            criterion = nn.CrossEntropyLoss(reduction='none')
+            optim_backbone = optim.SGD(models['backbone'].parameters(), lr=LR,
+                                       momentum=MOMENTUM, weight_decay=WDECAY)
+            optim_module = optim.SGD(models['module'].parameters(), lr=LR,
+                                     momentum=MOMENTUM, weight_decay=WDECAY)
             sched_backbone = lr_scheduler.MultiStepLR(optim_backbone, milestones=MILESTONES)
-            sched_module   = lr_scheduler.MultiStepLR(optim_module, milestones=MILESTONES)
+            sched_module = lr_scheduler.MultiStepLR(optim_module, milestones=MILESTONES)
 
             optimizers = {'backbone': optim_backbone, 'module': optim_module}
             schedulers = {'backbone': sched_backbone, 'module': sched_module}
 
             # Training and test
-            train(models, criterion, optimizers, schedulers, dataloaders, EPOCH, EPOCHL, vis, plot_data)
+            train(models, criterion, optimizers, schedulers, dataloaders, EPOCH, EPOCHL, vis, plot_data, checkpoint_dir)
             acc = test(models, dataloaders, mode='test')
-            print('Trial {}/{} || Cycle {}/{} || Label set size {}: Test acc {}'.format(trial+1, TRIALS, cycle+1, CYCLES, len(labeled_set), acc))
+            print('Trial {}/{} || Cycle {}/{} || Label set size {}: Test acc {}'.format(trial + 1, TRIALS, cycle + 1, CYCLES, len(labeled_set), acc))
 
             ##
             #  Update the labeled dataset via loss prediction-based uncertainty measurement
 
-            # Randomly sample 10000 unlabeled data points
-            random.shuffle(unlabeled_set)
-            subset = unlabeled_set[:SUBSET]
+            # Randomly sample from unlabeled data points
+            random.shuffle(unlabeled_indices)
+            subset = unlabeled_indices[:min(SUBSET, len(unlabeled_indices))]
+
+            if len(subset) == 0:
+                print('남은 비라벨 데이터가 없어 사이클을 종료합니다.')
+                break
 
             # Create unlabeled dataloader for the unlabeled subset
-            unlabeled_loader = DataLoader(cifar10_unlabeled, batch_size=BATCH, 
-                                          sampler=SubsetSequentialSampler(subset), # more convenient if we maintain the order of subset
+            unlabeled_loader = DataLoader(unlabeled_set, batch_size=BATCH,
+                                          sampler=SubsetSequentialSampler(subset),
                                           pin_memory=True)
 
             # Measure uncertainty of each data points in the subset
@@ -262,20 +320,21 @@ if __name__ == '__main__':
 
             # Index in ascending order
             arg = np.argsort(uncertainty)
-            
+
+            add_count = min(ADDENDUM, len(subset))
             # Update the labeled dataset and the unlabeled dataset, respectively
-            labeled_set += list(torch.tensor(subset)[arg][-ADDENDUM:].numpy())
-            unlabeled_set = list(torch.tensor(subset)[arg][:-ADDENDUM].numpy()) + unlabeled_set[SUBSET:]
+            labeled_set += list(torch.tensor(subset)[arg][-add_count:].numpy())
+            unlabeled_indices = list(torch.tensor(subset)[arg][:-add_count].numpy()) + unlabeled_indices[len(subset):]
 
             # Create a new dataloader for the updated labeled dataset
-            dataloaders['train'] = DataLoader(cifar10_train, batch_size=BATCH, 
-                                              sampler=SubsetRandomSampler(labeled_set), 
+            dataloaders['train'] = DataLoader(train_set, batch_size=BATCH,
+                                              sampler=SubsetRandomSampler(labeled_set),
                                               pin_memory=True)
-        
+
         # Save a checkpoint
         torch.save({
                     'trial': trial + 1,
                     'state_dict_backbone': models['backbone'].state_dict(),
                     'state_dict_module': models['module'].state_dict()
                 },
-                './cifar10/train/weights/active_resnet18_cifar10_trial{}.pth'.format(trial))
+                os.path.join(checkpoint_dir, 'active_resnet18_{}_trial{}.pth'.format(dataset_name, trial)))
